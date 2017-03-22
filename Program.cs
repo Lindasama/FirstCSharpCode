31a134bf111d1b74a8ed33fc4635195030f95fda
徒桜 <Lindasama.net@outlook.com>
2017-03-23 07:35:39 +0800

    Exercise

--- Program.cs
+++ Program.cs
@@ -4,34 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CSharp_One_Project
+namespace Practice1
 {
+    /// <summary>
+    /// static表示“静态的”。 string表示“字符串”数据类型。 using常常写在类的前面，用来引用其他命名空间中的类
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Fuck The World!");
-            //int a = Convert.ToInt32(Console.ReadLine());
-            //if (a > 1)
-            //    Console.WriteLine("What The Fuck");
-            //else
-            //    Console.WriteLine("Fuck The World");
-            //Console.WriteLine(a);
-            double firstNumber, secondNumber;
-            string userName;
-            Console.WriteLine("Enter your name:");//请输入你的名字
-            userName = Console.ReadLine();
-            Console.WriteLine($"Welcome{userName}!");//打印你的名字
-            Console.WriteLine("Now give me a number:");//请给我第一个数字
-            firstNumber = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Now give me another number:");//请给我第二个数字
-            secondNumber = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine($"The sum of {firstNumber} and {secondNumber} is " + $"{firstNumber + secondNumber}.");//神**加法
-            Console.WriteLine($"The result of subtracting {secondNumber} from " + $"{firstNumber} is {firstNumber - secondNumber}.");//神**减法
-            Console.WriteLine($"The product of {firstNumber} and {secondNumber} " + $"is {firstNumber * secondNumber}.");//神**乘法
-            Console.WriteLine($"The result of dividing {firstNumber} by {secondNumber} " + $"is {firstNumber / secondNumber}.");//神**除法
-            Console.WriteLine($"The remainder after dividing {firstNumber} by {secondNumber} is " + $"{firstNumber % secondNumber}.");//神**取余
-            Console.ReadKey();
+            bool b = true;
+
+            Console.WriteLine(b);
+
+            int days = DateTime.Now.DayOfYear;
+
+
+            b = (days % 2 == 0);
+
+            if (b)
+            {
+                Console.WriteLine("days is an even number");
+            }
+            else
+            {
+                Console.WriteLine("days is an odd number");
+            }
         }
     }
 }
